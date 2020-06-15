e963a3dfb85c0d41f4b7e9ad289cffa54246ac9d
Dmitry <dmit.insait@gmail.com>
2020-06-15 18:58:55 +1000

    Это первый коммит

---
+++ site/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log ();
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log (persone.name);
+
+let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
+
+console.log (arr[1]);
+
+// alert("Hello World");
+
+// let answer = confirm ("Are you here?");
+
+// console.log (answer);
+
+
+let answer = prompt ("есть ли вам 18?");
+
+console.log (typeof(arr));
